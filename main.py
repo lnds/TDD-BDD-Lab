1b8c0a2315989d33fd719846a0b732b7a943d332
Eduardo Díaz <ediaz@dcc8090.cl>
2021-05-27 19:25:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 def create_app(config):
+  print('creando app')
   app = Flask(__name__)
+  print(app)
   app.config.from_object(config)
   from models import db
   db.init_app(app)
@@ -10,9 +13,20 @@ def create_app(config):
 class Config:
   DEBUG = False
   SQLALCHEMY_DATABASE_URI = 'sqlite:///books.db'
-  
+
 app = create_app(Config())
-  
+
+from models import Book
+
+@app.route('/')
+def index():
+  books = Book.query.all()
+  return render_template('index.html', books= books)
+
+@app.route('/detail//<int:book_id>')
+def detail(book_id):
+  book = Book.query.get(book_id)
+  return render_template('detail.html', book=book)
+
 if __name__ == "__main__":
-  from views import *
   app.run(host='0.0.0.0', port=8080)
